49a1dc9ece4cfe4ac3ced5bcf8cd0519173dbfd4
opgalarza <opgalarza@hotmail.com>
2020-06-03 21:08:55 -0400

    Functional Interfaces

---
+++ src/functionalinterfaces/ConsumerExample.java
@@ -0,0 +1,13 @@
+package functionalinterfaces;
+
+import java.util.function.Consumer;
+
+public class ConsumerExample {
+
+	public static void main(String[] args) {
+		
+		Consumer<String> c1 = (s) -> System.out.println(s.toUpperCase());
+		c1.accept("omar");
+	}
+
+}
